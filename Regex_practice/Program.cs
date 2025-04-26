e9997199e9f5779aaaf86c7d7a62ddacb4bf82a1
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-26 15:54:26 -0600

    More Regex practice

--- Regex_practice/Program.cs
+++ Regex_practice/Program.cs
@@ -32,7 +32,7 @@ string text = "Mark bought 3 apples for 10$. Mike has 25.50$. Start 12345 End. @
             // 1) Извлечь все слова длиной ровно 4 символа.
             //string p = @"\b\w{4}\b";
             //MatchCollection matches = Regex.Matches(text, p);
-            //foreach (Match match in  matches ) 
+            //foreach (Match match in matches)
             //{
             //    Console.WriteLine(match.ToString());
             //}
@@ -69,12 +69,90 @@ string text = "Mark bought 3 apples for 10$. Mike has 25.50$. Start 12345 End. @
             //    Console.WriteLine(match.ToString());
             //}
 
-            //6) Проверить, является ли введенная строка числом.
-            string p = "^\\d+$";
-            bool res = Regex.IsMatch(text, p);
-                Console.WriteLine(res);
+            // 6) Проверить, является ли введенная строка числом.
+            //string p = "^\\d+$";
+            //bool res = Regex.IsMatch(text, p);
+            //    Console.WriteLine(res);
 
+            // 7) Вставить "Ок" после каждой буквы "о" в тексте.
+            //string pattern = "o";
+            //string replacement = "oOk";
 
+            //string result = Regex.Replace(text, pattern, replacement);
+
+            //Console.WriteLine(result);
+
+            // 8) Удалить все цифры из текста.
+            //string pattern = @"\d";
+            //string replacement = "";
+            //string result = Regex.Replace(text, pattern, replacement);
+            //Console.WriteLine(result);
+
+            // 9) Проверить, содержит ли текст цифры.
+            //string pattern = @"\d";
+            //bool containsDigits = Regex.IsMatch(text, pattern);
+            //Console.WriteLine("Does the text include digits? " + containsDigits);
+
+            // 10) Определить, состоит ли строка только из букв или только из цифр.
+            //bool onlyLetters = Regex.IsMatch(text, @"^[A-Za-z]+$");
+            //bool onlyDigits = Regex.IsMatch(text, @"^\d+$");
+
+            //if (onlyLetters)
+            //    Console.WriteLine("Text includes only letters.");
+            //else if (onlyDigits)
+            //    Console.WriteLine("Text incudes only digits.");
+            //else
+            //    Console.WriteLine("Text includes letters, digits or other characters.");
+
+            // 11) Заменить последовательности точек на одну точку.
+            //string text2 = "This is a test... Wait.. Another one.... Done.";
+            //string pattern = @"\.{2,}";
+            //string replacement = ".";
+
+            //string result = Regex.Replace(text2, pattern, replacement);
+
+            //Console.WriteLine(result);
+
+            // 12) Проверить надежность пароля (не менее 8 символов, минимум одна заглавная, одна строчная буква и одна цифра).
+            //string password = "Secure123"; 
+            //string password2 = "Password";
+            //string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$";
+
+            //bool isStrong = Regex.IsMatch(password, pattern);
+
+            //Console.WriteLine("Is the password strong? " + isStrong);
+            //bool isStrong2 = Regex.IsMatch(password2, pattern);
+
+            //Console.WriteLine("Is the password strong? " + isStrong2);
+
+            // 13) Удалить введенный символ из строки, независимо от регистра.
+            //string text2 = "Example Text With Mixed CASE Letters.";
+            //char toRemove = 'e'; // Character to remove (case-insensitive)
+
+            //string pattern = Regex.Escape(toRemove.ToString());
+            //string result = Regex.Replace(text2, pattern, "", RegexOptions.IgnoreCase);
+
+            //Console.WriteLine(result);
+
+            // 14) Извлечь из строки вещественное число с фиксированной точкой.
+            //string text2 = "Mark bought 3 apples for 10$. Mike has 25.50$. Start 12345 End.";
+            //string pattern = @"\d+\.\d+";
+
+            //MatchCollection matches = Regex.Matches(text2, pattern);
+
+            //foreach (Match match in matches)
+            //{
+            //    Console.WriteLine("Found float: " + match.Value);
+            //}
+
+            // 15) Заменить слово "fish" на "123", избегая изменения схожих слов.
+            string text2 = "I saw a fish. Then I saw a fishing rod and a catfish.";
+            string pattern = @"\bfish\b";
+            string replacement = "123";
+
+            string result = Regex.Replace(text2, pattern, replacement);
+
+            Console.WriteLine(result);
         }
     }
 }
